33a120ebd2226e1c0139be03ad4c336186fa3c07
riccardo-negri <67798955+riccardo-negri@users.noreply.github.com>
2022-03-17 00:07:24 +0100

    Cleaned code, it's possible to reduce the number of states but then it's less readable

--- vivado-project.srcs/sources_1/new/project.vhd
+++ vivado-project.srcs/sources_1/new/project.vhd
@@ -33,29 +33,23 @@ type internal_state_type is (S00, S01, S10, S11);
 
 signal state : state_type;
 signal internal_state : internal_state_type;
-signal w_word_index : integer range 0 to 7;
-signal shift : integer range 0 to 7;
+signal finished_reading : boolean;
+signal w_word_index, shift : integer range 0 to 7;
 signal w_word, words_number, words_left, z_word : std_logic_vector(7 downto 0);
 signal read_addr, write_addr : std_logic_vector(15 downto 0);
-signal word_ready_to_write, finished_reading : boolean;
 
 begin
-
     process(i_clk, i_rst)
     begin
         if i_rst = '1' then
             state <= START;
-            read_addr <= (others => '0');
-            write_addr <= "0000001111101000";
             o_done <= '0';
-            finished_reading <= false;
             
         elsif rising_edge(i_clk) then
             case state is
                 when START =>
                     if(i_start='1') then
                         o_en <= '0';
-                        o_we <= '0';
                         internal_state <= S00;
                         finished_reading <= false;
                         read_addr <= (others => '0');
@@ -64,17 +58,17 @@ begin
                     end if;
                 
                 when GET_WORDS_NUMBER =>
-                    read_addr <= read_addr + 1;
                     o_en <= '1';
                     o_we <= '0';
                     o_address <= (others => '0');
+                    read_addr <= read_addr + 1;
                     state <= WAIT_WORDS_NUMBER;
                 
                 when WAIT_WORDS_NUMBER =>
+                    o_en <= '0';
                     state <= FETCH_WORDS_NUMBER;
                     
                 when FETCH_WORDS_NUMBER =>
-                    words_number <= i_data;
                     words_left <= i_data;
                     if (i_data = 0) then
                         state <= DONE;
@@ -82,28 +76,27 @@ begin
                         state <= CALL_READ;
                     end if;
                     
-                    
                 when CALL_READ =>
-                    o_address <= read_addr;
-                    read_addr <= read_addr + 1;
                     o_en <= '1';
                     o_we <= '0';
+                    o_address <= read_addr;
+                    read_addr <= read_addr + 1;
                     state <= WAIT_READ;
                     
                 when WAIT_READ =>
+                    o_en <= '0';
                     state <= FETCH_READ;
                     
-                when FETCH_READ => 
-                    o_en <= '0';
+                when FETCH_READ =>
                     w_word <= i_data;                    
                     -- setup encoder
-                    -- internal_state <= S00;
                     z_word <= "00000000";
                     w_word_index <= 7;
                     shift <= 6;
                     state <= ENCODER_1_TO_2; 
                     
                 when ENCODER_1_TO_2 =>
+                    o_en <= '0';
                     case internal_state is
                         when S00 =>
                             if w_word(w_word_index) = '0' then
@@ -157,11 +150,10 @@ begin
                         w_word_index <= w_word_index - 1;
                         shift <= 6;
                         state <= WRITE;
-                    elsif (w_word_index = 0) then
-                                               
-                        state <= WRITE;
+                    elsif (w_word_index = 0) then                      
                         finished_reading <= true;
                         words_left <= words_left -1;
+                        state <= WRITE;
                     else
                         w_word_index <= w_word_index - 1;
                         state <= ENCODER_1_TO_2;
@@ -172,15 +164,15 @@ begin
                     o_we <= '1';
                     o_address <= write_addr;
                     o_data <= z_word;
-                    z_word <= "00000000";
                     write_addr <= write_addr + "0000000000000001";
+                    
                     if (words_left = 0) then
                         state <= DONE;
                     elsif (finished_reading) then
                         state <= CALL_READ;
-                        -- internal_state <= S00; 
                         finished_reading <= false;
                     else
+                        z_word <= "00000000";
                         state <= ENCODER_1_TO_2;
                     end if;
                     
@@ -194,6 +186,7 @@ begin
                         o_done <= '0';
                         state <= START;
                     end if;
+                
             end case;      
         end if;      
     end process;
